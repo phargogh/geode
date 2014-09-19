d5d6bf790dd80cc688414cbd6ae1c4d780a8bba8
James Douglass <jamesdouglassusa@gmail.com>
2014-09-19 13:57:02 -0700

    Adding subprocess call to env/pip

--- geode.py
+++ geode.py
@@ -1,7 +1,15 @@
 import subprocess
+import os
 
 if __name__ == '__main__':
     # set up virtualenv
-    subprocess.call(['virtualenv --clear geode_env'], shell=True)
+
+    envdir = 'geode_env'
+
+    subprocess.call(['virtualenv --clear %s' % envdir], shell=True)
     print 'finished creating virtualenv:geode_env'
 
+    pip_path = os.path.join(envdir, 'bin', 'pip')
+
+    # Must refer to pip using explicit path
+    subprocess.call('%s install wheel' % pip_path, shell=True)
