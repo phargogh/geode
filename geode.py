c3d901e3f5c473c59bd0fde1a59b0c582c290eb7
James Douglass <jamesdouglassusa@gmail.com>
2014-09-19 14:11:14 -0700

    Installing wheel, saving locally for later

--- geode.py
+++ geode.py
@@ -4,6 +4,10 @@ import os
 if __name__ == '__main__':
     # set up virtualenv
 
+    local_package_dir = os.path.expanduser('~/.geode/packages/')
+    if not os.path.exists(local_package_dir):
+        os.makedirs(local_package_dir)
+
     envdir = 'geode_env'
 
     subprocess.call(['virtualenv --clear %s' % envdir], shell=True)
@@ -12,4 +16,8 @@ if __name__ == '__main__':
     pip_path = os.path.join(envdir, 'bin', 'pip')
 
     # Must refer to pip using explicit path
-    subprocess.call('%s install wheel' % pip_path, shell=True)
+    kwargs = {
+        'pip': pip_path,
+        'download_dir': local_package_dir,
+    }
+    subprocess.call('%(pip)s install wheel --download-cache %(download_dir)s' % kwargs, shell=True)
